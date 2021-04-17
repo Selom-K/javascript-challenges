3c6122807b666eb74f557c49f177791bf8e29da8
Selom <selomk@hotmail.com>
2021-04-17 17:36:06 -0500

    Updated data table for the 2nd Folder

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,29 @@
+console.log("app.js loaded")
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// UFO Events
+function tableDisplay(ufoEvents) {
+    var tbody = d3.select("tbody");
+    ufoEvents.forEach((ufoRecord) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoRecord).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.html(value);
+      });
+    });
+  };
+
+  // Remove data from the table
+function deleteTbody() {
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+  };
+    
+  // Show UFO Events
+  console.log(tableData);
+  tableDisplay(tableData);
