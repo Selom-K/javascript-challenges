aeee042849aa5d7766db9ba94a45fcce7cf4e211
Selom <selomk@hotmail.com>
2021-04-17 16:53:27 -0500

    1st Folder Update One

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
+console.log("app.js loaded")
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Display UFO Events
+function tableDisplay(ufoEvents) {
+    var tbody = d3.select("tbody");
+    ufoEvents.forEach((ufoRecord) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoRecord).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.html(value);
+      });
+    });
+  };
